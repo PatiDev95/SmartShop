ca2e13968c289f35661cc6e4b9ac90e37249f4b5
PatiDev95 <54117153+PatiDev95@users.noreply.github.com>
2020-10-22 22:35:21 +0200

    Add new test.

--- HappyShop.Tests/ProductTest.cs
+++ HappyShop.Tests/ProductTest.cs
@@ -1,5 +1,6 @@
 using HappyShop.Core.Domain;
 using System;
+using System.Security.Cryptography.X509Certificates;
 using Xunit;
 
 namespace HappyShop.Tests
@@ -45,6 +46,39 @@ namespace HappyShop.Tests
             Assert.Equal("ubrania", product.Category);
             Assert.Equal("w cêtki", product.Description);
             Assert.Equal(ProductCondition.New, product.ProductCondition);
+        }
+
+        [Fact]
+        public void WhenCreatingANewProductANameShorterThan5CharactersShouldResultInAnError()
+        {
+            //Arrange and Act, Assert
+            Assert.Throws<Exception>(() => new Product("ubrania", 120, "Pi¿", "w cêtki", ProductCondition.New));
+        }
+
+        [Fact]
+        public void WhenCreatingANewProductASpecialCharacterInTheNameShouldThrowAnError()
+        {
+            //Arrange and Act, Assert
+            Assert.Throws<Exception>(() => new Product("ubrania", 120, "Pi¿@mk@", "w cêtki", ProductCondition.New));
+        }
+
+        [Fact]
+        public void blablabla()
+        {
+            
+
+            //Arrange and Act, Assert
+            Assert.Throws<Exception>(() => new Product("ubrania", 120, "Pi¿@mk@", "w cêtki", ProductCondition.New));
+        }
+
+        public void CreateLongText()
+        {
+            string dlugiTekst = "bla bla bla";
+            for (int i = 0; i < 100; i++)
+            {
+                dlugiTekst += dlugiTekst;
+            }
+
         }
     }
 }
